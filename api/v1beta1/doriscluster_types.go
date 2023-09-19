f2099a6b3f9a69fb026726334d968c78815bd381
Linying Assad <linying@apache.org>
2023-09-19 18:07:43 +0800

    Completed DorisCluster.spec CRD.

--- api/v1beta1/doriscluster_types.go
+++ api/v1beta1/doriscluster_types.go
@@ -17,31 +17,14 @@ limitations under the License.
 package v1beta1
 
 import (
+	apps "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// DorisClusterSpec defines the desired state of DorisCluster
-type DorisClusterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of DorisCluster. Edit doriscluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-}
-
-// DorisClusterStatus defines the observed state of DorisCluster
-type DorisClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
-
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-
-// DorisCluster is the Schema for the dorisclusters API
+// DorisCluster is the Schema for the doris clusters API
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 type DorisCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -50,13 +33,165 @@ type DorisCluster struct {
 	Status DorisClusterStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
-
 // DorisClusterList contains a list of DorisCluster
+// +kubebuilder:object:root=true
 type DorisClusterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []DorisCluster `json:"items"`
+}
+
+// DorisClusterSpec describes the attributes that a user creates on a Doris cluster.
+// +k8s:openapi-gen=true
+type DorisClusterSpec struct {
+	Fe         *FeSpec         `json:"fe,omitempty"`
+	Be         *BeSpec         `json:"be,omitempty"`
+	Cn         *CnSpec         `json:"cn,omitempty"`
+	Broker     *BrokerSpec     `json:"broker,omitempty"`
+	HadoopConf *HadoopConfSpec `json:"hadoopConf,omitempty"`
+
+	// Doris cluster image version
+	Version string `json:"version"`
+
+	// ImagePullPolicy of Doris cluster Pods
+	// +optional
+	ImagePullPolicy corev1.PullPolicy `json:"imagePullPolicy,omitempty"`
+
+	// ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling
+	// any of the images.
+	// +optional
+	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty"`
+
+	// Annotations of Doris cluster pods that will be merged with component annotation settings.
+	// +optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// Affinity for pod scheduling of Doris cluster.
+	// +optional
+	Affinity *corev1.Affinity `json:"affinity,omitempty"`
+
+	// Tolerations are applied to Doris cluster pods, allowing pods to be scheduled onto nodes with matching taints.
+	// +optional
+	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
+
+	// Specify pod priorities of pods in Doris cluster, default to empty.
+	// +optional
+	PriorityClassName string `json:"priorityClassName,omitempty"`
+
+	// Update strategy of Doris cluster StatefulSet.
+	// +optional
+	StatefulSetUpdateStrategy apps.StatefulSetUpdateStrategyType `json:"statefulSetUpdateStrategy,omitempty"`
+}
+
+// FeSpec contains details of FE members.
+// +k8s:openapi-gen=true
+type FeSpec struct {
+	DorisComponentSpec `json:",inline"`
+
+	// The storageClassName of the persistent volume for TiDB data storage.
+	// Defaults to Kubernetes default storage class.
+	// +optional
+	StorageClassName string `json:"storageClassName,omitempty"`
+
+	// Service defines a Kubernetes service of FE
+	Service *FeServiceSpec `json:"service,omitempty"`
+}
+
+// BeSpec contains details of BE members.
+// +k8s:openapi-gen=true
+type BeSpec struct {
+	DorisComponentSpec `json:",inline"`
+
+	// The storageClassName of the persistent volume for TiDB data storage.
+	// Defaults to Kubernetes default storage class.
+	// +optional
+	StorageClassName string `json:"storageClassName,omitempty"`
+}
+
+// CnSpec contains details of CN members.
+// +k8s:openapi-gen=true
+type CnSpec struct {
+	DorisComponentSpec `json:",inline"`
+}
+
+// BrokerSpec contains details of Broker members.
+// +k8s:openapi-gen=true
+type BrokerSpec struct {
+	DorisComponentSpec `json:",inline"`
+}
+
+// HadoopConfSpec contains the configuration needed for doris to connect to the Hadoop cluster.
+// +k8s:openapi-gen=true
+type HadoopConfSpec struct {
+	// Hostname-IP mapping of Hadoop cluster nodes.
+	Hosts []HostnameIpItem `json:"hostAliases"`
+	// Hadoop configuration files.
+	Config map[string]string `json:"config,omitempty"`
+}
+
+// HostnameIpItem define Hostname-IP kv item
+// +k8s:openapi-gen=true
+type HostnameIpItem struct {
+	Ip   string `json:"ip"`
+	Name string `json:"name"`
+}
+
+// FeServiceSpec defines `.fe.service` field of `DorisCluster.spec`.
+// +k8s:openapi-gen=true
+type FeServiceSpec struct {
+	// Type of the real kubernetes service
+	Type corev1.ServiceType `json:"type,omitempty"`
+
+	// ExternalTrafficPolicy of the service
+	// Optional: Defaults to omitted
+	// +optional
+	ExternalTrafficPolicy *corev1.ServiceExternalTrafficPolicyType `json:"externalTrafficPolicy,omitempty"`
+
+	// Expose the FE query port
+	// Optional: Defaults to 0
+	// +optional
+	QueryPort *int `json:"queryPort,omitempty"`
+
+	// Expose the FE http port
+	// Optional: Defaults to 0
+	// +optional
+	HttpPort *int `json:"httpPort,omitempty"`
+}
+
+// DorisComponentSpec is the base component spec.
+// +k8s:openapi-gen=true
+type DorisComponentSpec struct {
+	//Base image of the component
+	BaseImage string `json:"baseImage"`
+
+	// Type of the real kubernetes service
+	Version string `json:"version"`
+
+	// The desired ready replicas
+	// +kubebuilder:validation:Minimum=0
+	Replicas int32 `json:"replicas"`
+
+	// Defines the specification of resource cpu, mem, storage.
+	corev1.ResourceRequirements `json:",inline"`
+
+	// Additional Doris component configuration
+	// Ref:
+	// - https://doris.apache.org/docs/dev/admin-manual/config/fe-config/
+	// - https://doris.apache.org/docs/dev/admin-manual/config/be-config/
+	// +optional
+	Configs map[string]string `json:"config,omitempty"`
+
+	// HostAliases is an optional list of hosts and IPs that will be injected into the pod's hosts
+	// file if specified.
+	// +optional
+	HostAliases []corev1.HostAlias `json:"hostAliases,omitempty"`
+
+	// Additional component spec
+	K8sNativeComponentSpec `json:",inline"`
+}
+
+// DorisClusterStatus defines the observed state of DorisCluster
+type DorisClusterStatus struct {
 }
 
 func init() {
---
+++ api/v1beta1/types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+// K8sNativeComponentSpec is the native K8s pod attributes specification.
+// +k8s:openapi-gen=true
+type K8sNativeComponentSpec struct {
+
+	// Annotations of Doris cluster pods that will be merged with component annotation settings.
+	// +optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// Affinity for pod scheduling of Doris cluster.
+	// +optional
+	Affinity *corev1.Affinity `json:"affinity,omitempty"`
+
+	// Tolerations are applied to Doris cluster pods, allowing pods to be scheduled onto nodes with matching taints.
+	// +optional
+	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
+
+	// Specify pod priorities of pods in Doris cluster, default to empty.
+	// +optional
+	PriorityClassName string `json:"priorityClassName,omitempty"`
+
+	// Update strategy of Doris cluster StatefulSet.
+	// +optional
+	StatefulSetUpdateStrategy appsv1.StatefulSetUpdateStrategyType `json:"statefulSetUpdateStrategy,omitempty"`
+
+	// Additional environment variables to set in the container
+	// +optional
+	AdditionalEnvs []corev1.EnvVar `json:"env,omitempty"`
+
+	// Additional containers of the component.
+	// +optional
+	AdditionalContainers []corev1.Container `json:"additionalContainers,omitempty"`
+
+	// Additional volumes of component pod.
+	// +optional
+	AdditionalVolumes []corev1.Volume `json:"additionalVolumes,omitempty"`
+
+	// Additional volume mounts of component pod.
+	// +optional
+	AdditionalVolumeMounts []corev1.VolumeMount `json:"additionalVolumeMounts,omitempty"`
+}
