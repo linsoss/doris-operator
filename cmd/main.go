7450a4a30834932f6e7eea2044862b07b9088f6e
Linying Assad <linying@apache.org>
2023-11-25 17:57:27 +0800

    Don‘t start the Autoscale controller when Kubernetes <= 1.22

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"k8s.io/apimachinery/pkg/version"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"os"
+	"os/user"
+	"path"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -64,6 +71,11 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
+	// Obtain kubernetes server version
+	serverVersion := obtainK8sServerVersion()
+	setupLog.Info(fmt.Sprintf("Kubernetes version: %s, platform: %s", serverVersion, serverVersion.Platform))
+
+	// Setup manager
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
@@ -77,6 +89,8 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Setup controllers
+	setupLog.Info("set up DorisCluster controller")
 	if err = (&controller.DorisClusterReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
@@ -84,6 +98,8 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DorisCluster")
 		os.Exit(1)
 	}
+
+	setupLog.Info("set up DorisInitializer controller")
 	if err = (&controller.DorisInitializerReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
@@ -91,13 +107,8 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DorisInitializer")
 		os.Exit(1)
 	}
-	if err = (&controller.DorisAutoscalerReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "DorisAutoscaler")
-		os.Exit(1)
-	}
+
+	setupLog.Info("set up DorisMonitor controller")
 	if err = (&controller.DorisMonitorReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
@@ -105,8 +116,23 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DorisMonitor")
 		os.Exit(1)
 	}
+
+	// Setup autoscaler controller when Kubernetes version >= 1.22
+	if serverVersion != nil && serverVersion.Major >= "1" && serverVersion.Minor >= "22" {
+		setupLog.Info("set up DorisAutoscaler controller")
+		if err = (&controller.DorisAutoscalerReconciler{
+			Client: mgr.GetClient(),
+			Scheme: mgr.GetScheme(),
+		}).SetupWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create controller", "controller", "DorisAutoscaler")
+			os.Exit(1)
+		}
+	} else {
+		setupLog.Info("do not set up DorisAutoscaler controller because Kubernetes version < 1.22")
+	}
 	//+kubebuilder:scaffold:builder
 
+	// Manager health & ready check
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
@@ -121,4 +147,46 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+// Attempt to obtain the current Kubernetes server version.
+func obtainK8sServerVersion() *version.Info {
+	config, err := findK8sConfig()
+	if err != nil {
+		setupLog.Error(err, "unable to set up Kubernetes config")
+		os.Exit(1)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		setupLog.Error(err, "unable to create Kubernetes clientset")
+		os.Exit(1)
+	}
+	serverVersion, err := clientset.Discovery().ServerVersion()
+	if err != nil {
+		setupLog.Error(err, "unable to get server version")
+		os.Exit(1)
+	}
+	return serverVersion
+}
+
+// Find the target Kubernetes configuration.
+func findK8sConfig() (*rest.Config, error) {
+	var config *rest.Config
+	var err error
+
+	config, err = rest.InClusterConfig()
+	if err == nil {
+		return config, nil
+	}
+	// fallback to user‘s default kubeconfig path
+	var usr *user.User
+	usr, err = user.Current()
+	if err != nil {
+		return nil, err
+	}
+	config, err = clientcmd.BuildConfigFromFlags("", path.Join(usr.HomeDir, ".kube/config"))
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
 }
